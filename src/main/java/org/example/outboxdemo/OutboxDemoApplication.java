9fa46b30139796b30218858ae29c63687614b679
nllsdfx <nllsdfx@gmail.com>
2026-05-05 10:47:52 +0500

    first commit

---
+++ src/main/java/org/example/outboxdemo/OutboxDemoApplication.java
@@ -0,0 +1,14 @@
+package org.example.outboxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class OutboxDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OutboxDemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/outboxdemo/OutboxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.outboxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutboxDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
